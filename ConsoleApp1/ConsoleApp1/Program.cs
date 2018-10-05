dd895d8fabb4df88e4ef1eb736604bfd5e0bd7df
tanakorn128 <60030041@kmitl.ac.th>
2018-10-05 14:05:02 +0700

    ok

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string value = client.DownloadString("http://www.kmitl.ac.th");
+            Console.WriteLine("---WebClient result---");
+            Console.WriteLine(value.Length);
+            Console.WriteLine(value);
+            StreamWriter outtxt = new StreamWriter("webload.html");
+            outtxt.Write(value);
+            outtxt.Dispose();
+        }
+    }
+}
