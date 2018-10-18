2a5dfa7bd2d77c675f83614d72f9309e079d6663
tanakorn128 <60030041@kmitl.ac.th>
2018-10-18 23:20:59 +0700

    sucess

---
+++ Lab6_1/Lab6_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int val = 999;
+            refMethod(ref val);
+            Console.WriteLine(val);
+            Console.ReadLine();
+        }
+
+        static void refMethod(ref int i)
+        {
+            i = i + 1;
+        }
+
+
+
+
+    }
+}
---
+++ หาเลขคู่เลขคี่/หาเลขคู่เลขคี่/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Out(input());
+            Console.ReadKey();
+
+        }
+        static int input()
+        {
+            int num;
+            Console.WriteLine("ENTER NUMBER");
+            int.TryParse(Console.ReadLine(), out num);
+            return num;
+        }
+        static void Out(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("EVEN");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
