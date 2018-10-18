53256c52de9f6e3add1d271f9f7e253122026485
tanakorn128 <60030041@kmitl.ac.th>
2018-10-18 23:35:51 +0700

    nosucess

---
+++ เมธอดวนลอบหาเลข7/เมธอดวนลอบหาเลข7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace เมธอดวนลอบหาเลข7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            searching(1,2,3,4,5,7,14,21);
+        }
+        static void searching (params int[] list)
+        {
+            for (int num = 0; num <= list.Length; num++)
+            {
+                if (num % 7 == 0)
+                {
+                    Console.WriteLine(list[num] + "");
+                }
+            }
+            
+        }
+    }
+}
