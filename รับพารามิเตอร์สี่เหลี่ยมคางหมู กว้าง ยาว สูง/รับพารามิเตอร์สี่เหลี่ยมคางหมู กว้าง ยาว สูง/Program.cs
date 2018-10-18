df689f9bc43f5e40ed2ab3a15a193723a686b907
tanakorn128 <60030041@kmitl.ac.th>
2018-10-18 23:44:30 +0700

    sucess

---
+++ รับพารามิเตอร์สี่เหลี่ยมคางหมู กว้าง ยาว สูง/รับพารามิเตอร์สี่เหลี่ยมคางหมู กว้าง ยาว สูง/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace รับพารามิเตอร์สี่เหลี่ยมคางหมู_กว้าง_ยาว_สูง
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = pik(0.5,7,5);
+            Console.WriteLine(k);
+        }
+        static double pik(params double[] i)
+        {
+            double sum = 1;
+            foreach (double j in i)
+                sum *= j;
+            return sum;
+
+        }
+    }
+}
